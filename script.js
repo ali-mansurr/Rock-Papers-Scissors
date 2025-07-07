bafe82e2220ea9e1f55d96c37b5b7afc8146ca89
ali-mansurr <197986189+ali-mansurr@users.noreply.github.com>
2025-07-07 15:16:52 +0330

    feat: Add getComputerChoice function
    Added a function for random Rock/Paper/Scissors selection

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function getComputerChoice () {
+    let randomInt = getRandomInt(3);
+    let computerChoice = undefined;
+    switch (randomInt) {
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break;
+        default:
+            break;
+    }
+    return computerChoice;
+}
