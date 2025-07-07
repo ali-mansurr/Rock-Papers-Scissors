201767f6311ac08e8edde5afd2a7d147d49193ca
ali-mansurr <197986189+ali-mansurr@users.noreply.github.com>
2025-07-07 15:37:43 +0330

    feat: Add getHumanChoice function
    Added a function for getting the player's choice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getComputerChoice () {
+function getComputerChoice() {
     let randomInt = getRandomInt(3);
     let computerChoice = undefined;
     switch (randomInt) {
@@ -20,3 +20,35 @@ function getComputerChoice () {
     }
     return computerChoice;
 }
+
+function getHumanChoice() {
+    let input = prompt("Choose your move: \n1. Rock \n2. Paper \n3. Sciccors \nEnter the number of your choice:")
+    let humanChoice = undefined;
+    switch (Number(input)) {
+        case 1:
+            humanChoice = "rock";
+            break;
+        case 2:
+            humanChoice = "paper";
+            break;
+        case 3:
+            humanChoice = "scissors";
+            break;
+        default:
+            alert("Invalid Input");
+            break;
+    }
+    return humanChoice;
+}
+
+console.log(getHumanChoice())
+
+// function playRound (humanChoice, computerChoice) {
+//     if(humanChoice === computerChoice) {
+
+//     }
+// }
+
+// let humanScore = 0;
+// let computerScore = 0;
+
